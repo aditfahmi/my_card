04a0798ac1f5bbf8eae17c6267b18ff5089931ae
Aditya Fahmi Aprianto <adityafahmiaprianto@gmail.com>
2022-11-08 11:35:37 +0700

    Make stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+//! Dalam file ini berisi pembeleajaran tentang bagaimana membuat beautifull UI dengan Flutter widget
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+// ?Pembuatan statless widget untuk menampung code dan dapat menjalankan code sembari menampilkan dalam emulator dan kita bisa memanfaatkan fitur hot reload
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.redAccent.shade400,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
